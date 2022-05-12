585588752b9821bce57688ec01a1af50e97f420b
kilianm97 <kilian.mele@gmail.com>
2022-05-12 23:31:29 +0200

    fix calcDuration

--- app/methods/calcDuration.js
+++ app/methods/calcDuration.js
@@ -2,7 +2,7 @@ import isNotFinished from "app/methods/isNotFinished"
 
 export default function calcDuration(job, formattedState) {
   const dateDiff = isNotFinished(formattedState, job)
-    ? (Date.now() - new Date(job?.createdAt).getTime()) / 1000 / 60
+    ? (Date.now() - new Date(job?.updatedAt).getTime()) / 1000 / 60
     : (new Date(job?.events?.[4]?.time).getTime() - new Date(job?.createdAt).getTime()) / 1000 / 60
   return Math.abs(dateDiff).toFixed(2)
 }
