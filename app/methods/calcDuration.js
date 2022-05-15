5aded3a40ff8f9b51d88e97beae86d2f59d4d72c
kilianm97 <kilian.mele@gmail.com>
2022-05-15 21:52:10 +0200

    fix duration format

--- app/methods/calcDuration.js
+++ app/methods/calcDuration.js
@@ -2,7 +2,18 @@ import isNotFinished from "app/methods/isNotFinished"
 
 export default function calcDuration(job, formattedState) {
   const dateDiff = isNotFinished(formattedState, job)
-    ? (Date.now() - new Date(job?.updatedAt).getTime()) / 1000 / 60
-    : (new Date(job?.events?.[4]?.time).getTime() - new Date(job?.createdAt).getTime()) / 1000 / 60
-  return Math.abs(dateDiff).toFixed(2)
+    ? Date.now() - new Date(job?.updatedAt).getTime()
+    : new Date(job?.events?.[4]?.time).getTime() - new Date(job?.createdAt).getTime()
+
+  console.log(dateDiff / 1000)
+  let seconds = Math.floor(dateDiff / 1000)
+  let minutes = Math.floor(seconds / 60)
+  let hours = Math.floor(minutes / 60)
+
+  seconds = seconds % 60
+  minutes = minutes % 60
+
+  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`
 }
