b076ee34dca4ca5aa0a6e98de008152d9d8d287d
kilianm97 <kilian.mele@gmail.com>
2022-05-16 22:24:03 +0200

    fallback date picker on safari

--- app/core/components/job-filter/index.js
+++ app/core/components/job-filter/index.js
@@ -41,9 +41,19 @@ export default function JobFilter({ filterJob, jobs }) {
         <option value="error">Error</option>
       </Form.Select>
       <Form.Label htmlFor="createdAt">createdAt</Form.Label>
-      <Form.Control type="date" name="createdAt"></Form.Control>
+      <Form.Control
+        type="date"
+        name="createdAt"
+        placeholder="dd/mm/yyyy"
+        pattern="(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)"
+      ></Form.Control>
       <Form.Label htmlFor="updatedAt">updatedAt</Form.Label>
-      <Form.Control type="date" name="updatedAt"></Form.Control>
+      <Form.Control
+        type="date"
+        name="updatedAt"
+        placeholder="dd/mm/yyyy"
+        pattern="(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)"
+      ></Form.Control>
       <Form.Label htmlFor="duration">duration</Form.Label>
       <Form.Control name="duration" type="number" min="0" max="10"></Form.Control>
       <Button type="submit">Filter</Button>
