c5f28a02c85d86970bb15a876fbbf024feae8d78
kilianm97 <kilian.mele@gmail.com>
2022-05-17 16:01:58 +0200

    update lastlogin

--- app/auth/mutations/login.js
+++ app/auth/mutations/login.js
@@ -1,50 +1,51 @@
-import { resolver, SecurePassword, AuthenticationError } from "blitz";
-import db from "db";
-import { Login } from "../validations";
+import { resolver, SecurePassword, AuthenticationError } from "blitz"
+import db from "db"
+import { Login } from "../validations"
 export const authenticateUser = async (rawEmail, rawPassword) => {
-  const {
-    email,
-    password
-  } = Login.parse({
+  const { email, password } = Login.parse({
     email: rawEmail,
-    password: rawPassword
-  });
+    password: rawPassword,
+  })
   const user = await db.user.findFirst({
     where: {
-      email
-    }
-  });
-  if (!user) throw new AuthenticationError();
-  const result = await SecurePassword.verify(user.hashedPassword, password);
+      email,
+    },
+  })
+  if (!user) throw new AuthenticationError()
+  const result = await SecurePassword.verify(user.hashedPassword, password)
 
   if (result === SecurePassword.VALID_NEEDS_REHASH) {
     // Upgrade hashed password with a more secure hash
-    const improvedHash = await SecurePassword.hash(password);
+    const improvedHash = await SecurePassword.hash(password)
     await db.user.update({
       where: {
-        id: user.id
+        id: user.id,
       },
       data: {
-        hashedPassword: improvedHash
-      }
-    });
+        hashedPassword: improvedHash,
+      },
+    })
   }
+  const lastLoginDate = new Date()
 
-  const {
-    hashedPassword,
-    ...rest
-  } = user;
-  return rest;
-};
-export default resolver.pipe(resolver.zod(Login), async ({
-  email,
-  password
-}, ctx) => {
+  await db.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      lastLogin: lastLoginDate,
+    },
+  })
+
+  const { hashedPassword, ...rest } = user
+  return rest
+}
+export default resolver.pipe(resolver.zod(Login), async ({ email, password }, ctx) => {
   // This throws an error if credentials are invalid
-  const user = await authenticateUser(email, password);
+  const user = await authenticateUser(email, password)
   await ctx.session.$create({
     userId: user.id,
-    role: user.role
-  });
-  return user;
-});
+    role: user.role,
+  })
+  return user
+})
