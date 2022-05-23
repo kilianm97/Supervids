814df37a10c505b238ce23dba4017c8d29d404ac
kilianm97 <kilian.mele@gmail.com>
2022-05-23 11:02:11 +0200

    remove useless files

--- app/auth/validations.js
+++ app/auth/validations.js
@@ -34,23 +34,6 @@ export const Login = z.object({
   email,
   password: z.string(),
 })
-export const ForgotPassword = z.object({
-  email,
-})
-export const ResetPassword = z
-  .object({
-    password: password,
-    passwordConfirmation: password,
-    token: z.string(),
-  })
-  .refine((data) => data.password === data.passwordConfirmation, {
-    message: "Passwords don't match",
-    path: ["passwordConfirmation"], // set the path of the error
-  })
-export const ChangePassword = z.object({
-  currentPassword: z.string(),
-  c: password,
-})
 export const DeleteUser = z.object({
   id: z.number(),
 })
